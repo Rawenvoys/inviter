307d807b601140b4916800bab48365269ccfc68e
Rawenvoys <lukwis95@gmail.com>
2023-01-20 17:19:54 +0100

    Testing parameters in Blazor

---
+++ Inviter/Inviter.Application/Services/IInvitationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inviter.Application.Services
+{
+    public interface IInvitationService
+    {
+        Task Find(Guid code);
+    }
+}
---
+++ Inviter/Inviter.Web.Guest/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Inviter.Web.Guest.Pages
+{
+    [Route("/Zaproszenie/{code:guid}")]
+    public partial class Index
+    {
+        [Parameter]
+        public Guid Code { get; set; }
+
+        //protected override void OnInitialized()
+        //{
+        //    Text ??= "fantastic";
+        //}
+    }
+}
