dff7862ed394fdaf2c569e5cfdbef1fed0b0e469
AIGCRDEV\lwisniewski <lukasz.wisniewski@santanderconsumer.pl>
2023-12-04 14:59:07 +0100

    Implement static galery

---
+++ Inviter/Inviter.Application/Models/GuestAppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inviter.Application.Models;
+public class GuestAppSettings
+{
+    public const string SecionName = "AppSettings";
+    public string GaleryPathUrl { get; set; }
+    public bool DisplayGaleryPage { get; set; }
+}
---
+++ Inviter/Inviter.Web.Guest/Pages/Galery.razor.cs
@@ -0,0 +1,20 @@
+﻿using Inviter.Application.Commands.GetImages;
+using Inviter.Application.Models;
+using Inviter.Domain.Aggregate;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+
+namespace Inviter.Web.Guest.Pages
+{
+    public partial class Galery
+    {
+        [Inject]
+        public IOptions<GuestAppSettings> _appSettings { get; set; }
+
+        public string GaleryPathUrl;
+
+        protected override async Task OnInitializedAsync() 
+            => GaleryPathUrl = _appSettings?.Value.GaleryPathUrl ?? throw new Exception();
+
+    }
+}
